ac5d21a9da1eb240bb5c600584e6fd2fdbdd29c9
SaraCastrillon <castrillonsara@gmail.com>
2015-03-25 17:51:50 -0500

    Objeto TO Espacio

--- Conferre_Data/src/co/com/eafit/conferre/conferencias/data/to/EspacioTO.java
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/to/EspacioTO.java
@@ -3,5 +3,19 @@ package co.com.eafit.conferre.conferencias.data.to;
 import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
 
 public class EspacioTO implements ObjetoTO {
+	private String nombre;
+	private Boolean disponible;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Boolean getDisponible() {
+		return disponible;
+	}
+	public void setDisponible(Boolean disponible) {
+		this.disponible = disponible;
+	}
 
 }

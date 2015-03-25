8b275a2904d8fb76d6e03cd5a1cf71908a89cb9b
m0rgan <miguelmoreno93@gmail.com>
2015-03-25 16:45:56 -0500

    daos

---
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/ListaDeEsperaDAO.java
@@ -0,0 +1,34 @@
+package co.com.eafit.conferre.conferencias.data.dac;
+
+import java.util.Collection;
+
+import co.com.eafit.conferre.conferencias.data.base.DAOGenerico;
+import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+
+public class ListaDeEsperaDAO implements DAOGenerico {
+
+	@Override
+	public ObjetoTO crear(ObjetoTO parametro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<ObjetoTO> recuperar(ObjetoTO parametros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ObjetoTO update(ObjetoTO nuevoObjeto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int borrar(ObjetoTO objetoaBorrar) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/SillasDAO.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package co.com.eafit.conferre.conferencias.data.dac;
+
+import java.util.Collection;
+
+import co.com.eafit.conferre.conferencias.data.base.DAOGenerico;
+import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+
+/**
+ * @author Moreno
+ *
+ */
+public class SillasDAO implements DAOGenerico {
+
+	/* (non-Javadoc)
+	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#crear(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
+	 */
+	@Override
+	public ObjetoTO crear(ObjetoTO parametro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#recuperar(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
+	 */
+	@Override
+	public Collection<ObjetoTO> recuperar(ObjetoTO parametros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#update(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
+	 */
+	@Override
+	public ObjetoTO update(ObjetoTO nuevoObjeto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#borrar(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
+	 */
+	@Override
+	public int borrar(ObjetoTO objetoaBorrar) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

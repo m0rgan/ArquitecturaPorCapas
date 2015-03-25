9b106fb943ac3eafe5587bb1fdf519694f6e7ef7
SaraCastrillon <castrillonsara@gmail.com>
2015-03-25 17:33:00 -0500

    Actualizacion SillasDAO

--- Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/SillasDAO.java
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/SillasDAO.java
@@ -3,10 +3,16 @@
  */
 package co.com.eafit.conferre.conferencias.data.dac;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
 
 import co.com.eafit.conferre.conferencias.data.base.DAOGenerico;
 import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+import co.com.eafit.conferre.conferencias.data.to.ConferenciaTO;
+import co.com.eafit.conferre.conferencias.data.to.SillasTO;
 
 /**
  * @author Moreno
@@ -14,18 +20,32 @@ import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
  */
 public class SillasDAO implements DAOGenerico {
 
-	/* (non-Javadoc)
-	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#crear(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
-	 */
+	java.sql.Connection  conn;
+	
+	public SillasDAO(Connection conn2) {
+		this.conn = conn2;
+	}
+	
+	
 	@Override
 	public ObjetoTO crear(ObjetoTO parametro) {
-		// TODO Auto-generated method stub
-		return null;
+		SillasTO sillas = null;
+		
+		try {
+			sillas = (SillasTO) parametro;
+			PreparedStatement prep = conn.prepareStatement("INSERT INTO Sillas values(?,?)");
+			prep.setString(1, sillas.getNumero());
+			prep.setString(2, sillas.getTipo());
+			
+			int resultado = prep.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return sillas;
 	}
 
-	/* (non-Javadoc)
-	 * @see co.com.eafit.conferre.conferencias.data.base.DAOGenerico#recuperar(co.com.eafit.conferre.conferencias.data.base.ObjetoTO)
-	 */
+	
 	@Override
 	public Collection<ObjetoTO> recuperar(ObjetoTO parametros) {
 		// TODO Auto-generated method stub

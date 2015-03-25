dd2b51c243164f7822524db8fe9238d90e89224e
aniramirez2 <anita-0621@hotmail.com>
2015-03-25 17:48:55 -0500

    dao eventos medio hecho

--- Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/EspacioDAO.java
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/dac/EspacioDAO.java
@@ -1,16 +1,46 @@
 package co.com.eafit.conferre.conferencias.data.dac;
-
+import java.sql.Connection;
 import java.util.Collection;
-
 import co.com.eafit.conferre.conferencias.data.base.DAOGenerico;
 import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+import co.com.eafit.conferre.conferencias.data.base.DAOGenerico;
+import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+import co.com.eafit.conferre.conferencias.data.to.*;
 
 public class EspacioDAO implements DAOGenerico {
 
+java.sql.Connection  conn;
+	
+	public EspacioDAO(Connection connect) {
+		this.conn = connect;
+	}
+
 	@Override
 	public ObjetoTO crear(ObjetoTO parametro) {
-		// TODO Auto-generated method stub
-		return null;
+		EspacioTO espacio = null;
+		
+		try {
+			espacio = (EspacioTO) parametro;
+			PreparedStatement prep = conn.prepareStatement("INSERT INTO espacios values(?,?,?,?,?)");
+			prep.setString(1, espacio.getNombre());
+			prep.setString(2, espacio.getNombreConferencista());
+			prep.setString(3, espacio.getTipo());
+			
+			Date fecha = new Date(conf.getFecha().getTime());
+			prep.setDate(4, fecha);
+			prep.setInt(5, conf.getSillasDisponibles());
+			
+			int resultado = prep.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conf;
 	}
 
 	@Override

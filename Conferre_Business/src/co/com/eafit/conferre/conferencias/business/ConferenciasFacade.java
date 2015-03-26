7ea6f0d19bccdddb1d0a161474ba611ac5c79fd2
m0rgan <miguelmoreno93@gmail.com>
2015-03-25 20:39:53 -0500

    Presentation

--- Conferre_Business/src/co/com/eafit/conferre/conferencias/business/ConferenciasFacade.java
+++ Conferre_Business/src/co/com/eafit/conferre/conferencias/business/ConferenciasFacade.java
@@ -1,7 +1,12 @@
 package co.com.eafit.conferre.conferencias.business;
 
+import java.util.List;
+
+import co.com.eafit.conferre.conferencia.web.model.Espacio;
 import co.com.eafit.conferre.conferencias.data.to.ConferenciaTO;
 
 public interface ConferenciasFacade {
 	public ConferenciaTO crearConferencia(ConferenciaTO conferencia);
+
+	public List<Espacio> obtenerEspacios();
 }
---
+++ Conferre_Presentation/src/co/com/eafit/conferre/conferencia/web/model/Espacio.java
@@ -0,0 +1,19 @@
+package co.com.eafit.conferre.conferencia.web.model;
+
+public class Espacio {
+	private String code;
+	private String id;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}

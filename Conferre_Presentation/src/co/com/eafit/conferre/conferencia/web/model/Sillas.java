4366d5a6a17e04ccd3f097b7677433345f015147
SaraCastrillon <castrillonsara@gmail.com>
2015-03-30 20:42:43 -0500

    Clase Sillas
    Capa de presentación-modelo

---
+++ Conferre_Presentation/src/co/com/eafit/conferre/conferencia/web/model/Sillas.java
@@ -0,0 +1,35 @@
+package co.com.eafit.conferre.conferencia.web.model;
+import co.com.eafit.conferre.conferencias.data.to.SillasTO;
+
+public class Sillas {
+	
+	private String numero;
+	private String tipo;
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void actualizarSillas(SillasTO sillas){
+		numero = sillas.getNumero();
+		tipo = sillas.getTipo();
+		id = sillas.getId();
+	}
+
+}

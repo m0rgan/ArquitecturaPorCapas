4470d84b89655064447049d15112e8a9fa163332
SaraCastrillon <castrillonsara@gmail.com>
2015-03-31 00:51:01 -0500

    Controlador de Evento

---
+++ Conferre_Presentation/src/co/com/eafit/conferre/conferencias/web/controlador/ControladorDeEvento.java
@@ -0,0 +1,35 @@
+package co.com.eafit.conferre.conferencias.web.controlador;
+
+import co.com.eafit.conferre.conferencias.business.evento.RestEventoFacade;
+import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+import co.com.eafit.conferre.conferencias.data.to.EventoTO;
+import co.com.eafit.conferre.conferencias.web.model.Evento;
+import co.com.eafit.conferre.soporte.ExcepcionUnitOfWork;
+
+@ManagedBean
+public class ControladorDeEvento {
+	private Evento evento;
+	private RestEventoFacade restEventoFacade;
+	
+	  @PostConstruct
+	  public void init() {
+	    evento = new Evento();
+	    restEventoFacade = new RestEventoFacade();
+	  }
+	  
+	  public void crearUnNuevoAsistente() {
+		    EventoTO EventoTO = null;
+		    ObjetoTO Evento1TO;
+		    try {
+		      Evento1TO = restEventoFacade.crearEvento(Evento1TO);
+		      EventoTO = (EventoTO)Evento1TO;
+		      evento.actualizarEvento(EventoTO);
+		    }
+		    catch (ExcepcionUnitOfWork ex) {
+		      System.err.println("Error: " + ex.getMessage());
+		      
+		    }
+		    
+	  }
+
+}

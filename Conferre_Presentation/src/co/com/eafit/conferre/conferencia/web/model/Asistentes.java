c100e825d107448b3fe7ed9c8def6ed4e1a3fadd
SaraCastrillon <castrillonsara@gmail.com>
2015-03-30 20:25:28 -0500

    Clase Asistentes
    Capa de presentación-modelo

---
+++ Conferre_Presentation/src/co/com/eafit/conferre/conferencia/web/model/Asistentes.java
@@ -0,0 +1,44 @@
+package co.com.eafit.conferre.conferencia.web.model;
+import co.com.eafit.conferre.conferencias.data.to.AsistentesTO;
+
+
+public class Asistentes {
+
+	private String id;
+	private String nombre;
+	private String telefono;
+	private String correo;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public void actualizarAistentes(AsistentesTO asistentes){
+		id = asistentes.getId();
+		nombre = asistentes.getNombre();
+		telefono = asistentes.getTelefono();
+		correo = asistentes.getCorreo();
+	}
+	
+}

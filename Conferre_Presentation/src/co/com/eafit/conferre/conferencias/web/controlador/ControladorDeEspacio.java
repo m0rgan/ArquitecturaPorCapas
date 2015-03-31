528184f387291c135274c0fb2bf0ca048a26467d
SaraCastrillon <castrillonsara@gmail.com>
2015-03-31 00:46:10 -0500

    Controlado de Espacio

---
+++ Conferre_Presentation/src/co/com/eafit/conferre/conferencias/web/controlador/ControladorDeEspacio.java
@@ -0,0 +1,36 @@
+package co.com.eafit.conferre.conferencias.web.controlador;
+
+import co.com.eafit.conferre.conferencias.business.espacio.RestEspacioFacade;
+import co.com.eafit.conferre.conferencias.data.base.ObjetoTO;
+import co.com.eafit.conferre.conferencias.data.to.EspacioTO;
+import co.com.eafit.conferre.conferencias.web.model.Espacio;
+import co.com.eafit.conferre.soporte.ExcepcionUnitOfWork;
+
+@ManagedBean
+public class ControladorDeEspacio {
+	private Espacio espacio;
+	private RestEspacioFacade restEspacioFacade;
+	
+	  @PostConstruct
+	  public void init() {
+	    espacio = new Espacio();
+	    restEspacioFacade = new RestEspacioFacade();
+	  }
+	  
+	  public void crearUnNuevoAsistente() {
+		    EspacioTO EspacioTO = null;
+		    ObjetoTO Espacio1TO;
+		    try {
+		      Espacio1TO = restEspacioFacade.crearEspacio(Espacio1TO);
+		      EspacioTO = (EspacioTO)Espacio1TO;
+		      espacio.actualizarEspacio(EspacioTO);
+		    }
+		    catch (ExcepcionUnitOfWork ex) {
+		      System.err.println("Error: " + ex.getMessage());
+		      
+		    }
+		    
+	  }
+
+
+}

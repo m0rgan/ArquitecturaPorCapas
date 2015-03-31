5bd13deb49f5bca2258789ce9af815bbb914dc25
SaraCastrillon <castrillonsara@gmail.com>
2015-03-30 20:19:39 -0500

    Clase AsistentesTO

---
+++ Conferre_Data/src/co/com/eafit/conferre/conferencias/data/to/AsistentesTO.java
@@ -0,0 +1,34 @@
+package co.com.eafit.conferre.conferencias.data.to;
+
+public class AsistentesTO {
+	private String id;
+	private String nombre;
+	private String telefono;
+	private String correo;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+
+}

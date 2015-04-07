637ceb334f9c1e4c2cff56e6ea22a05d8c03b200
aniramirez2 <anita-0621@hotmail.com>
2015-04-06 22:51:55 -0500

    lo ultimito

--- Conferre_Business/src/co/com/eafit/conferre/conferencias/business/sillas/cantidadDeSillasAComprarUseCase.java
+++ Conferre_Business/src/co/com/eafit/conferre/conferencias/business/sillas/cantidadDeSillasAComprarUseCase.java
@@ -1,13 +1,27 @@
 package co.com.eafit.conferre.conferencias.business.sillas;
 
 import co.com.eafit.conferre.conferencias.data.to.ConferenciaTO;
+import co.com.eafit.conferre.conferencias.data.to.SillasTO;
 
 public class cantidadDeSillasAComprarUseCase {
 	
-	public void elegirCantidadDeSilla(int numero)
+	public void elegirCantidadDeSilla(int numero, SillasTO silla)
 	{
+		SillasTO sillas = new SillasTO();
+		listarSillasUseCase lsuc = new listarSillasUseCase();
+		if( lsuc.sillasdisponible.contains(silla)){
+			
+			System.out.println("Se reservará la silla para usted");
+		}else
+		{
+			System.out.println("la silla no esta disponible");
+		}
+	}
+	
+	public void modificarinventariosillas(int numero){
 		ConferenciaTO conf = new ConferenciaTO();
-		conf.setSillasDisponibles(-numero);
+		int nuevonum= conf.getSillasDisponibles()-numero;
+		conf.setSillasDisponibles(nuevonum);
 	}
 
 }
--- Conferre_Business/src/co/com/eafit/conferre/conferencias/business/sillas/listarSillasUseCase.java
+++ Conferre_Business/src/co/com/eafit/conferre/conferencias/business/sillas/listarSillasUseCase.java
@@ -1,10 +1,13 @@
 package co.com.eafit.conferre.conferencias.business.sillas;
+import java.util.List;
 
+import co.com.eafit.conferre.conferencias.data.to.SillasTO;
 import co.com.eafit.conferre.conferencias.business.evento.CrearEventoUseCase;
 
 public class listarSillasUseCase {
 	comprarSillasUseCase cus = new comprarSillasUseCase();
 	CrearSillasUseCase silla = new CrearSillasUseCase();
+	List<SillasTO> sillasdisponible = null;
 	public void mostrarSillas(){
 		
 		for(int i=1; i< silla.SillasCreadas.size(); i++){
@@ -20,6 +23,7 @@ public class listarSillasUseCase {
 								
 			}else
 			{
+				sillasdisponible.add(silla.SillasCreadas.get(i));
 				System.out.println(silla.SillasCreadas.get(i));
 			}
 		}
